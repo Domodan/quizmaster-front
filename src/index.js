8a0ce567563c36a190d46f5ca0b2449c7600a04d
domodan <ongomdaniel9@gmail.com>
2024-03-26 16:24:12 +0300

    added the Home page component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./assets/globals.css"; // Custom styles
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // Routing
+import { Provider } from "react-redux"; // Redux state management
+import { Toaster } from "react-hot-toast"; // Notification
+//import { store } from './store'; // Redux store
+
+import App from "./App"; // Root component
+import Home from "./pages/Home";
+
+// Creating a router with routes and elements
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPage message='404 | Page not found' />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      // {
+      //   path: "/play-quiz",
+      //   element: <PlayQuiz />
+      // },
+      // {
+      //   path: "/create-quiz",
+      //   element: <CreateQuiz />
+      // },
+      // {
+      //   path: "/my-quizzes",
+      //   element: <MyQuizzes />
+      // },
+      // {
+      //   path: "/view-quiz",
+      //   element: <ViewQuiz />,
+      //   errorElement: <ErrorPage message='Quiz not found' />,
+      //   loader: viewQuizLoader
+      // },
+      // {
+      //   path: "/edit-quiz",
+      //   element: <EditQuiz />,
+      //   errorElement: <ErrorPage message='Quiz not found' />,
+      //   loader: editQuizLoader
+      // }
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <Toaster
+        toastOptions={{
+          duration: 3000,
+          style: { borderRadius: "10px", background: "#2c3030", color: "#fff" },
+        }}
+        position="center"
+      />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
